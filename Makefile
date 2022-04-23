1161c1d733dc422a6c36d87dbebb78c43fb0eaf5
Thomas Rademaker <Thomas@Rademaker.io>
2022-04-23 15:30:41 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	swift build -c debug --triple wasm32-unknown-wasi
+
+hello: build
+	fastly compute serve --skip-build --file ./.build/debug/Hello.wasm
+
+proxy: build
+	fastly compute serve --skip-build --file ./.build/debug/Proxy.wasm
---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "starter-kit",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    dependencies: [
+         .package(url: "https://github.com/AndrewBarba/swift-compute-runtime", branch: "main"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "Hello",
+            dependencies: [.product(name: "Compute", package: "swift-compute-runtime")]
+        ),
+        .executableTarget(
+            name: "Proxy",
+            dependencies: [.product(name: "Compute", package: "swift-compute-runtime")]
+        ),
+        .executableTarget(
+            name: "Rest",
+            dependencies: [.product(name: "Compute", package: "swift-compute-runtime")]
+        )
+    ]
+)
