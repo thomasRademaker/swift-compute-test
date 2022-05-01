629f6e6a6952e6056f6eef9229935f89a782edd7
Thomas Rademaker <thomas@sparrowtek.com>
2022-05-01 08:17:15 -0400

    get stories from union

--- Sources/Rest/App.swift
+++ Sources/Rest/App.swift
@@ -12,11 +12,21 @@ struct App {
 }
 
 struct StoryRoutes {
+    enum StoryError: Error {
+        case badURL
+    }
+
     static func register(_ router: Router) {
         router.get("/stories", getStories)
     }
 
     static func getStories(req: IncomingRequest, res: OutgoingResponse) async throws {
-        try await res.status(.ok).send("stories")
+        guard let url = URL(string: "https://union.barstoolsports.com/v2/stories/latest?type=standard_post&page=1&limit25") else {
+            throw StoryError.badURL
+        }
+        let fetchRequest = FetchRequest(url)
+        let fetchResponse = try await fetch(fetchRequest)
+        res.status(fetchResponse.status).send(fetchResponse.body)
+        // try await res.status(.ok).send("stories")
     }
 }
